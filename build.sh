aeb7d9b00743350fac30e5a959d6dd9cc4384cbe
Evenflowz <andreyevenflow@gmail.com>
2025-10-08 06:52:13 +0300

    Workng 8

--- build.sh
+++ build.sh
@@ -5,9 +5,3 @@ set -o errexit
 # 1. Install Python dependencies
 echo "Installing Python dependencies..."
 pip install -r requirements.txt
-
-# 2. Compile the C++ extension
-# This command tells pip to run the setup.py file and build the C++ module
-# in a way that the Python application can find and import it.
-echo "Compiling C++ extension..."
-pip install .
